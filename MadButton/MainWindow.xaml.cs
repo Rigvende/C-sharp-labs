db6491ec3b45ff6135d98e9f9592cdf6c30ae2bf
Marianna <53390795+Rigvende@users.noreply.github.com>
2020-03-18 20:38:52 +0300

    Add files via upload

---
+++ MadButton/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MadButton
+{
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonYes_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Отлично!");
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point point = e.GetPosition(this);
+            Canvas.SetLeft(yes, point.X - yes.ActualWidth / 2);
+            Canvas.SetTop(yes, point.Y - yes.ActualHeight / 2);
+        }
+
+        private void ButtonNo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                return;
+            }
+            _ = e.GetPosition(this);
+            Canvas.SetLeft(no, random.NextDouble() * ((Content as Canvas).ActualWidth - 50));
+            Canvas.SetTop(no, random.NextDouble() * ((Content as Canvas).ActualHeight - 50));
+        }
+
+        private void ButtonNo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Не торопитесь, подумайте.");
+        }
+    }
+}
