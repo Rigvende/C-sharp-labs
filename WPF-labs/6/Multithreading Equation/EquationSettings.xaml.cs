9915ec2a8d5b66807c03c1eaf0f5a2de86662490
Marianna <53390795+Rigvende@users.noreply.github.com>
2020-07-22 14:15:05 +0300

    Add files via upload

---
+++ WPF-labs/6/Multithreading Equation/EquationSettings.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Multithreading_Equation
+{
+     public partial class EquationSettings : Window
+    {
+        readonly Regex validation = new Regex(@"\D");
+        public Double A { get; set; }
+        public Double B { get; set; }
+        public Double N { get; set; }
+
+        public EquationSettings()
+        {
+            InitializeComponent();
+        }
+
+        public void ButtonEnable(bool condition)
+        {
+            saveData.IsEnabled = condition;
+        }
+
+        public void SetEquationData(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(fieldA.Text))
+                {
+                    throw new Exception("Нижний предел интегрирования не заполнен");
+                }
+                else
+                {
+                    if (validation.IsMatch(fieldA.Text) == true)
+                    {
+                        throw new Exception("Все поля должны содержать целые числа");
+                    }
+                    else
+                    {
+                        A = double.Parse(fieldA.Text);
+                    }
+                }
+                if (string.IsNullOrEmpty(fieldB.Text))
+                {
+                    throw new Exception("Верхний предел интегрирования не заполнен");
+                }
+                else
+                {
+                    if (validation.IsMatch(fieldB.Text) == true)
+                    {
+                        throw new Exception("Все поля должны содержать целые числа");
+                    }
+                    else
+                    {
+                        B = double.Parse(fieldB.Text);
+                    }
+                }
+                if (string.IsNullOrEmpty(fieldN.Text))
+                {
+                    throw new Exception("Число разбиений не заполнено");
+                }
+                else
+                {
+                    if (validation.IsMatch(fieldN.Text) == true)
+                    {
+                        throw new Exception("Все поля должны содержать целые числа");
+                    }
+                    else
+                    {
+                        N = double.Parse(fieldN.Text);
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }        
+
+    }
+
+}
---
+++ WPF-labs/6/Multithreading Equation/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Multithreading_Equation
+{
+    public partial class MainWindow : Window
+    {
+        EquationSettings settings;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenSettings();
+        }
+
+        private void OpenSettings()
+        {
+            settings = new EquationSettings
+            {
+                Owner = this
+            };
+            settings.Show();
+        }
+
+        private void CommandCalculateExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (settings == null)
+            {
+                settings = new EquationSettings
+                {
+                    Owner = this
+                };
+            }
+            try
+            {
+                if (settings.A == 0.0 && settings.B == 0.0 && settings.N == 0.0)
+                {
+                    throw new Exception("Не заполнены данные!");
+                }
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+                getResult.IsEnabled = false;
+            setData.IsEnabled = false;
+            settings.ButtonEnable(false);
+            progressBar.Maximum = settings.N;
+            Thread thread = new Thread(Calculate);
+            thread.Start();
+        }
+
+        public void Calculate()
+        {
+            double A = settings.A;
+            double B = settings.B;
+            double N = settings.N;
+            double H = (B - A) / N;
+            double X = 0.0;
+            try
+            {
+                for (int I = 1; I < N; I++)
+                {
+                    double root = A + H * I;
+                    X += Math.Pow(root, 4);
+                    Dispatcher.Invoke(new Action(() =>
+                    {
+                        progressBar.Value = I + 1;
+                        textBar.Content = string.Format("{0:f0}%", (I + 1) * 100 / N);
+                    }));                
+                }
+            }
+            catch (System.Threading.Tasks.TaskCanceledException E)
+            {
+                MessageBox.Show(E.Message);
+            }
+            double result = H * X;
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate ()
+            {
+                resultField.Text = string.Format("{0:f2}", result);
+                getResult.IsEnabled = true;
+                setData.IsEnabled = true;
+                settings.ButtonEnable(true);
+            });
+        }
+
+    }
+
+}
